fa1397dc18773a6bd912d022977a078337016445
Austin <aokincaid@gmail.com>
2016-01-04 16:45:33 -0800

    Add templates for base and index with dynamic content rendered throughout

--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Austin'}  # fake user
-    return  '''
-            <html>
-              <head>
-                <title>Home Page</title>
-              </head>
-              <body>
-                <h1>Hello, ''' + user['nickname'] + '''</h1>
-              </body>
-            </html>
-            '''
+    posts = [                      # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return  render_template('index.html',
+                            title='Home',
+                            posts=posts,
+                            user=user)
