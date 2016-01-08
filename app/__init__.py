c5412cb64d5bd1e1418821454f67e3b320dcf71f
Austin <aokincaid@gmail.com>
2016-01-07 18:31:58 -0800

    Confirgure app for SQLite database use

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-# Putting the import at the end avoids the circular import error.
+# Putting the import at the end avoids a circular import error.
 from app import views
