5825f77495aa1ecc7b92eb05ea84eb34838c41bf
Austin <aokincaid@gmail.com>
2016-01-07 18:52:37 -0800

    Add script for creating database and built SQLite database

---
+++ db_create.py
@@ -0,0 +1,14 @@
+#!flask/bin/python
+# Set of little Python scripts that invoke the migration APIs. It's just using
+# the SQLAlchemy-migrate package command line tools and APIs to create databases
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
