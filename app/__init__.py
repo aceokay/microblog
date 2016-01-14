771b5c099be23fef58e2b86fe187e90f5f19de5b
Austin <aokincaid@gmail.com>
2016-01-13 21:19:52 -0800

    Update views with login view functions

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,12 @@ db = SQLAlchemy(app)
 
 # Putting the import at the end avoids a circular import error.
 from app import views, models
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 #!flask/bin/python
 from app import app
 app.run(debug=True)
+
+# start with: ./run.py
+# visit: http://localhost:5000
